14442b0a47631a84138ac9f0640cce37787d30b2
Godwin Obiomah <obiomahgodwin@gmail.com>
2025-10-02 07:42:11 +0100

    Fix display_balance formatting to 2 decimal places

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,18 +1,18 @@
-# bank_account.py
-
 class BankAccount:
     def __init__(self, initial_balance=0):
-        self.__account_balance = initial_balance  # private attribute
+        self.account_balance = initial_balance
 
     def deposit(self, amount):
-        self.__account_balance += amount
+        self.account_balance += amount
 
     def withdraw(self, amount):
-        if amount <= self.__account_balance:
-            self.__account_balance -= amount
+        if amount <= self.account_balance:
+            self.account_balance -= amount
             return True
         else:
             return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.__account_balance}")
+        """Display the current account balance"""
+        print(f"Current Balance: ${self.account_balance:.2f}")
+
